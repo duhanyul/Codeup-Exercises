fc79d946a5f6d80c71b7f500b8048d3400528782
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-19 09:20:35 -0500

    Added both for loop and fizzbuzz php exercise

---
+++ for.php
@@ -0,0 +1,6 @@
+<<?php
+$start = readline('Please enter a starting number:');
+$end = readline('PLease enter an ending number:');
+for ($start; $start <=$end ; $start++) {
+  echo $start . PHP_EOL;
+}
