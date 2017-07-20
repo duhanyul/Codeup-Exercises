50f762d8030751e3d38adfe71731c121bb941d67
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-20 14:05:47 -0500

    Small orginization changes

---
+++ forEach.php
@@ -0,0 +1,28 @@
+<?php
+$things = array('Sgt. Pepper', "11", null, [1,2,3], 3.14, "12 + 7", false, (string) 11);
+foreach ($things as $value) {
+  $type = gettype($value);
+  echo "The type is: $type" . PHP_EOL;
+}
+echo PHP_EOL;
+foreach ($things as $value) {
+  $type = gettype($value);
+  if (is_scalar($type)) {
+    echo "$type is scalar" . PHP_EOL;
+  }
+}
+
+echo PHP_EOL;
+
+foreach ($things as $value) {
+  if (is_array($value)) {
+      foreach ($value as $key) {
+        echo "$key" . PHP_EOL;
+      }
+  }if ($value === false) {
+    echo "false";
+  }if ($value === null) {
+    echo "null";
+  }
+  echo "$value" . PHP_EOL;
+}
