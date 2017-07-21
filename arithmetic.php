42c3d7f3d77e54f077e327acb3caf88e18004263
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-21 16:10:45 -0500

    Added completed functions exercise arithmetic.php

--- arithmetic.php
+++ arithmetic.php
@@ -19,9 +19,14 @@ function divide($a, $b)
 {
     return $a / $b;
 }
-$input1 = 4;W
+function modulus($a,$b)
+{
+  return $a % $b;
+}
+$input1 = 4;
 $input2 = 5;
 echo "The addition of $input1 and $input2 is equal to : " . add($input1,$input2) . PHP_EOL;
 echo "The subtraction of $input1 and $input2 is equal to : " . subtract($input1,$input2) . PHP_EOL;
 echo "The multiplication of $input1 and $input2 is equal to : " . multiply($input1,$input2) . PHP_EOL;
 echo "The division of $input1 and $input2 is equal to : " . divide($input1,$input2) . PHP_EOL;
+echo "The modulus of $input1 and $input2 is equal to : " . modulus($input1,$input2) . PHP_EOL;
