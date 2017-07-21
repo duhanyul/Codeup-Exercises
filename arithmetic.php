ee0abf915b2981d3328dffd48fbe598872582ea4
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-07-21 16:19:07 -0500

    Added the argumets porition to the arithmetic functions exercise

--- arithmetic.php
+++ arithmetic.php
@@ -2,29 +2,54 @@
 
 function add($a, $b)
 {
+  if (is_numeric($a) && is_numeric($b)) {
     return $a + $b;
+  }else {
+    return "Invalid inputs must be numeric";
+  }
 }
 
 function subtract($a, $b)
 {
+  if (is_numeric($a) && is_numeric($b)) {
     return $a - $b;
+  }else {
+    return "Invalid inputs must be numeric";
+  }
 }
 
 function multiply($a, $b)
 {
+  if (is_numeric($a) && is_numeric($b)) {
     return $a * $b;
+  }else {
+    return "Invalid inputs must be numeric";
+  }
 }
 
 function divide($a, $b)
 {
-    return $a / $b;
+  if (is_numeric($a) && is_numeric($b)) {
+    if ($b != 0) {
+      return $a / $b;
+    }else {
+      return "ERROR CANNOT DIVIDE BY ZERO";
+      break;
+    }
+  }else {
+    return "Invalid inputs must be numeric";
+  }
 }
 function modulus($a,$b)
 {
-  return $a % $b;
+  if (is_numeric($a) && is_numeric($b)) {
+    return $a + $b;
+  }else {
+    return "Invalid inputs must be numeric";
+  }
 }
 $input1 = 4;
-$input2 = 5;
+$input2 = 'bob';
 echo "The addition of $input1 and $input2 is equal to : " . add($input1,$input2) . PHP_EOL;
 echo "The subtraction of $input1 and $input2 is equal to : " . subtract($input1,$input2) . PHP_EOL;
 echo "The multiplication of $input1 and $input2 is equal to : " . multiply($input1,$input2) . PHP_EOL;
